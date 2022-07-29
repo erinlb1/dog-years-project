a57995e78d06b3a9636e42e0abbcb65bcd68f8ea
erinlb1 <110261632+erinlb1@users.noreply.github.com>
2022-07-29 17:18:43 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+const myAge = 26
+// I am 26 years old 
+
+var earlyYears = 2
+// to represent the first two years of a dog's life
+
+earlyYears = earlyYears * 10.5
+
+var laterYears = myAge - 2
+// to represent each year after the first two years of a dog's life
+
+laterYears = laterYears * 4
+// to calculate dog years accounted for in later years
+
+console.log(earlyYears + laterYears);
+
+const myAgeInDogYears = earlyYears + laterYears
+// to calculate my age in dog years
+
+const myName = 'Erin'.toLowerCase()
+// to create a variable of my name with lowercase letters
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+// to display this statement using my name, age in human years, and age in dog years
